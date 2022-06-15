dc3e7e77989dd6776b79a5d88e6b099fcd90ef9c
Lucas Trentin <lucas.mtrentin@hotmail.com>
2022-06-15 11:01:05 -0300

    sla

--- testes/teste_ssr/nextjs/teste_fake_store/pages/products/index.tsx
+++ testes/teste_ssr/nextjs/teste_fake_store/pages/products/index.tsx
@@ -1,9 +1,27 @@
-import { GetStaticProps, NextPage } from "next";
-import Image from "next/image";
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import Link from "next/link";
+import { API } from "../../context/api";
 
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  description: string;
+};
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await API.get("/products");
+  const products: Product[] = res.data
+
+  return {
+    props: {
+      products
+    }
+  };
+}
+
+const Home: NextPage = ({products}: InferGetStaticPropsType<typeof getStaticProps>) => {
 
   return (
     <div className="bg-white">
@@ -11,11 +29,11 @@ const Home: NextPage = () => {
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map(product => (
+          {products.map((product: Product) => (
             <Link href={`/products/${product.id}`} key={product.id}>
               <a className="group">
                 <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                  <Image src={product.image} alt="" className="w-full h-full object-center group-hover:opacity-75" />
+                  <img src={product.image} alt="" className="w-full h-full object-center group-hover:opacity-75" />
                 </div>
                 <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
                 <p className="mt-1 text-lg font-medium text-gray-900">R${(product.price).toFixed(2).replace('.', ',')}</p>
